508ae331bd7acd3cf8cfe586af31430552ded1f3
Shubham Sharma <shubhamsharmaa2526@gmail.com>
2026-04-30 22:28:24 +0530

    painting home page

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,75 @@
 // owl carousol 
 
-var owl = $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:25,
+// var owl = $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:25,
+//     nav: false,
+//     dots: false,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:2,
+//             nav:false
+//         },
+//         600:{
+//             items:3,
+//             nav:false
+//         },
+//         1000:{
+//             items:3,
+//             nav:false,
+//             loop:false
+//         }
+//     }
+// });
+
+// $('.custom-next').click(function() {
+//     owl.trigger('next.owl.carousel');
+// });
+
+// $('.custom-prev').click(function() {
+//     owl.trigger('prev.owl.carousel');
+// });
+
+
+
+// projects section
+
+
+
+$(document).ready(function () {
+
+  const $owl = $('#pc-owl');
+
+  /* INIT - SIMPLE */
+  $owl.owlCarousel({
+    items: 3,
+    margin: 25,
+    loop: false,      
+    // center: true, 
     nav: false,
     dots: false,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:2,
-            nav:false
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:3,
-            nav:false,
-            loop:false
-        }
+    smartSpeed: 500,
+    items:2,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 3 }
     }
-});
+  });
 
-$('.custom-next').click(function() {
-    owl.trigger('next.owl.carousel');
-});
+  /* NEXT / PREV */
+  $('#pc-next').on('click', function () {
+    $owl.trigger('next.owl.carousel');
+  });
 
-$('.custom-prev').click(function() {
-    owl.trigger('prev.owl.carousel');
+  $('#pc-prev').on('click', function () {
+    $owl.trigger('prev.owl.carousel');
+  });
+
+
+
+
 });
 
 
